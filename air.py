6fdcb0a4dd5c6c18e5e7086a80345057fbe6d2dd
YinBohao <932625923@qq.com>
2023-03-03 00:16:37 +0800

    图像绘制

--- air.py
+++ air.py
@@ -1,27 +1,30 @@
 import os,sys
 os.chdir(sys.path[0])
 import csv
-from datetime import datetime
 from matplotlib import pyplot as plt
-import pandas as pd
-import numpy as np
 
 filename = r'data/(2023-02-21 22_49_18_2023-02-28 22_49_18).csv'
+import csv
 
-with open(filename) as f:
+with open(filename,"r") as f:
     reader = csv.reader(f)
+    dates,NMHCs,fumes,PMs = [],[],[],[]
     for i,header_rows in enumerate(reader):
         if i == 2:
             header_row = header_rows
-    print(header_row)
-    print('title')
-    # for index,column_header in enumerate(header_now):
-    #     print(index,column_header)
-    dates,NMHCs,fumes,PMs = [],[],[],[]
-    for row in reader:
-        dates.append(row[0])
-        NMHCs.append(row[2])
-        fumes.append(row[3])
-        PMs.append(row[4])
-    print(dates)
+        if i in range(3,100):
+            dates.append(header_rows[0])
+            NMHCs.append(header_rows[2])
+            fumes.append(header_rows[3])
+            PMs.append(header_rows[4])
     f.close()
+
+plt.plot(dates, NMHCs, color='r', label='NMHC')
+plt.plot(dates, fumes, color='g', label='fume')
+plt.plot(dates, PMs, color='b', label='PM')
+xlabel=[str(val)+'_1' for val in range(100,110)]
+ax = plt.gca()
+ax.set_xticklabels(xlabel)
+plt.legend()
+plt.xticks([])
+plt.show()
