380cc3b594e4e9e3f578219d1dc55f9a93c95a50
YinBohao <932625923@qq.com>
2023-03-06 17:40:26 +0800

    0306添加注释

--- analysis.py
+++ analysis.py
@@ -9,29 +9,26 @@ import warnings
 import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
-import numpy as np
 
 warnings.filterwarnings("ignore")
 
 filename1 = r'data/(2023-02-20 00_00_00_2023-02-26 00_00_00).csv'
 filename2 = r'data/(2023-02-27 00_00_00_2023-03-05 00_00_00).csv'
-
-
+#读取csv文件的0，2，3，4列，并且忽略其前两行表名，从列名所在行开始
 df_orig = pd.read_csv(filename2, usecols=[0, 2, 3, 4], skiprows=[
                       0, 1], encoding='gbk')
+#将上报时间列的字符串数据类型转化成时间戳
 df_orig['上报时间'] = pd.to_datetime(df_orig['上报时间'])
+#将读取数据按每小时进行分组
 df_data_hour = df_orig.groupby(pd.Grouper(key= '上报时间', axis=0, freq='H')).mean()
-
+#绘图
 plt.figure(figsize=(10,3))
 plt.plot(df_data_hour['非甲烷总烃(mg/m3)'],color='r', label='NMHC')
 plt.plot(df_data_hour['油烟浓度(mg/m3)'],color='g', label='fume')
 plt.plot(df_data_hour['颗粒物(mg/m3)'],color='b', label='PM')
 # plt.xticks(rotation = 30)
 plt.legend()
+#保存文件
+plt.savefig('(2023-02-27 00_00_00_2023-03-05 00_00_00)')
 plt.show()
 
-# for name in df_data_hour.columns:
-#     fig, axs = plt.subplots(1, 1, figsize=(10, 2))
-#     axs.plot(df_data_hour[name], color='blue')
-#     axs.set_title(name)
-#     plt.show()
--- test.py
+++ test.py
@@ -1,9 +1,27 @@
 import torch
-print('torch ',torch.__version__)
+import numpy as np
+import time
 
-print('cuda ',torch.cuda.is_available(),torch.version.cuda)
+X = torch.randn(20000, 20000)
+Y = torch.randn(20000, 20000)
 
-print('cudnn ',torch.backends.cudnn.is_available()
-      ,torch.backends.cudnn.version())
+start1 = time.time()
+# numpy内积
+Z = np.dot(X, Y)
+end1 = time.time()
+print (end1-start1)
 
-print(torch.cuda.get_arch_list())
+start2 = time.time()
+# torch内积
+Z = X.mm(Y)
+end2 = time.time()
+print (end2-start2)
+
+# print('torch ',torch.__version__)
+
+# print('cuda ',torch.cuda.is_available(),torch.version.cuda)
+
+# print('cudnn ',torch.backends.cudnn.is_available()
+#       ,torch.backends.cudnn.version())
+
+# print(torch.cuda.get_arch_list())
