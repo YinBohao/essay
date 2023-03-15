f9a40f8d2f8a21cc5c0779ef7b52a2dc4b649b44
YinBohao <932625923@qq.com>
2023-03-15 09:25:03 +0800

    箱型图3.0_时刻_日_周

--- K.py
+++ K.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn
+import datetime
 
 import os
 import sys
@@ -10,34 +11,97 @@ os.chdir(sys.path[0])
 
 matplotlib.rc("font", family='Microsoft YaHei')
 
-filename = r'data/监测历史数据.xls'
-# nrows8428行，3月1日-
-df_orig = pd.read_excel(
-    filename, usecols=[0, 6], nrows=65076, sheet_name='第一水质净化厂进水1')
+
+# def which_day(date):
+#     return datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%A')
+
+
+def re_series(grouped):
+    return grouped.reset_index()['cod'].reindex(grouped.reset_index()['cod'].index[::-1]).reset_index(drop=True)
+
+
+# filename = r'data/监测历史数据.xls'
+filename = r'data/result1.csv'
+df_boxplot = pd.read_csv(filename)
+# df = pd.read_excel(filename, usecols=[0, 6], sheet_name=[0, 1, 2])
+# 139267*2
+# df_orig = pd.concat([df[i] for i in range(3)], ignore_index=True)
+
+# 按周分组，去除不够一周的天数
+# df_boxplot = df_boxplot.iloc[1284:].reset_index()
+
 # 将上报时间列的字符串数据类型转化成时间戳
-df_orig['日期'] = pd.to_datetime(df_orig['日期'])
+# df_boxplot['datetime'] = pd.to_datetime(df_boxplot['datetime'])
 # 将读取数据按每小时进行分组
-df_grouped = df_orig.groupby(pd.Grouper(key='日期', axis=0, freq='D'))['cod'].mean()
-re_series=df_grouped.reset_index()['cod'].reindex(df_grouped.reset_index()['cod'].index[::-1]).reset_index(drop=True)
 
-df_boxplot = df_orig.copy()
-df_boxplot['date'] = df_boxplot['日期'].dt.strftime('%Y-%m-%d')
+df_grouped_H = df_boxplot.groupby('hour')['cod'].mean()
+df_grouped_H = df_grouped_H[df_grouped_H.notnull()]
 
-for name in df_boxplot.columns:
-    if name not in ['日期', 'date']:
-        fig, axs = plt.subplots(1, 1, figsize=(15, 10),sharex=True,sharey=True)
-        seaborn.boxplot(y=name, x='date', data=df_boxplot,
-                        flierprops={'marker': 'o',  # 异常值形状
-                                    'markerfacecolor': 'red',  # 形状填充色
-                                    'color': 'black',  # 形状外廓颜色
-                                    },
-                        showmeans=True,
-                        meanline=True,)
-        plt.xticks(rotation=45)
-        # plt.show()
+# df_boxplot['hour'] = df_boxplot['datetime'].dt.hour
 
-# group_name = [gn for gn in df_grouped.groups.keys()]
-plt.plot(re_series, color='g', label='mean_value')
-# plt.xticks([])
-plt.legend()
+# 将读取数据按天进行分组
+# df_grouped_D = df_orig.groupby(pd.Grouper(
+#     key='datetime', axis=0, freq='D'))['cod'].mean()
+# df_grouped_D = df_grouped_D[df_grouped_D.notnull()]
+
+
+# df_boxplot = df_orig.copy()
+# df_boxplot['date'] = df_boxplot['datetime'].dt.strftime('%Y-%m-%d')
+# df_boxplot['weekday'] = df_boxplot['datetime'].apply(lambda x: x.weekday())
+
+# 将读取数据按周进行分组
+# df_grouped_W = df_boxplot.groupby(
+#     pd.Grouper(key='datetime', axis=0, freq='W'))['date']
+
+
+# def which_week(date):
+#     for key, value in df_grouped_W:
+#         if date in list(value):
+#             a = str(key)[0:10]
+#     return a
+
+
+# df_boxplot['week'] = df_boxplot['date'].apply(lambda x: which_week(x))
+# df_boxplot['which_day'] = df_boxplot['weekday'].apply(
+#     lambda x: 'workday' if x < 5 else 'dayoff')
+
+# df_boxplot.to_csv(r'data/result1.csv', index=False)
+# 测试
+# for i in df_boxplot['work']:
+#     print(i,type(i))
+# 按时刻分组,箱型图
+seaborn.boxplot(y='cod', x='hour', data=df_boxplot,
+                flierprops={'marker': 'o',  # 异常值形状
+                            'markerfacecolor': 'red',  # 形状填充色
+                            'color': 'black',  # 形状外廓颜色
+                            },
+                showmeans=True,
+                meanline=True,)
+plt.plot(df_grouped_H, color='Navy', label='mean_value')
+
+# 按周分组,箱型图
+# seaborn.boxplot(y='cod', x='week', data=df_boxplot,
+#                         hue=df_boxplot['which_day'],
+#                         flierprops={'marker': 'o',  # 异常值形状
+#                                     'markerfacecolor': 'red',  # 形状填充色
+#                                     'color': 'black',  # 形状外廓颜色
+#                                     },
+#                         showmeans=True,
+#                         meanline=True,).invert_xaxis()
+
+# # 按天分组,箱型图
+# seaborn.boxplot(y='cod', x='date', data=df_boxplot,
+#                 flierprops={'marker': 'o',  # 异常值形状
+#                             'markerfacecolor': 'red',  # 形状填充色
+#                             'color': 'black',  # 形状外廓颜色
+#                             },
+#                 showmeans=True,
+#                 meanline=True,).invert_xaxis()
+# plt.plot(re_series(df_grouped_D), color='g', label='mean_value')
+
+
+plt.legend(fontsize=15)
+# plt.xticks(rotation=90)
+plt.ylabel('COD',fontsize=15)
+plt.xlabel('hour',fontsize=15)
 plt.show()
--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
-import numpy as np
-data = np.random.normal((3, 5, 4), (1.25, 1.00, 1.25), (100, 3))
-print(data)
+import pandas as pd
+import os
+import sys
+os.chdir(sys.path[0])
+
+filename = r'data/监测历史数据.xls'
+df = pd.ExcelFile(filename)
+
+
+df_concat= pd.concat([ pd.read_excel(df, sheet) for sheet in df.sheet_names])
+#将所有sheet中数据合并到一个df中
