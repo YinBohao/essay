94e27473f1e068eb858f942ed26546b99089e3c3
YinBohao <932625923@qq.com>
2023-03-16 14:16:13 +0800

    备份

--- draw.py
+++ draw.py
@@ -34,18 +34,48 @@ plt.boxplot(df_data_hour['cod'])
 # plt.xticks(rotation = 30)
 plt.legend()
 plt.show()
-# 按天分组，箱型图
-# for name in df_boxplot.columns:
-#     if name not in ['日期', 'date']:
-#         fig, axs = plt.subplots(1, 1, figsize=(15, 8),sharex=True,sharey=True)
-#         seaborn.boxplot(y=name, x='date', data=df_boxplot,
-#                         flierprops={'marker': 'o',  # 异常值形状
-#                                     'markerfacecolor': 'red',  # 形状填充色
-#                                     'color': 'black',  # 形状外廓颜色
-#                                     },
-#                         showmeans=True,
-#                         meanline=True,)
-#         plt.xticks(rotation=45)
-#         # plt.show()
+
 
 # # group_name = [gn for gn in df_grouped.groups.keys()]
+
+# def which_day(date):
+#     return datetime.datetime.strptime(date, '%Y-%m-%d').strftime('%A')
+
+
+# df = pd.read_excel(filename, usecols=[0, 6], sheet_name=[0, 1, 2])
+# 139267*2
+# df_orig = pd.concat([df[i] for i in range(3)], ignore_index=True)
+
+# 将读取数据按每小时进行分组
+
+# df_grouped_H = df_boxplot.groupby('hour')['cod'].mean()
+# 去重
+# df = df_boxplot.drop_duplicates(subset='datetime',keep='first',inplace=False,ignore_index=True)
+# df_grouped_H.to_csv(r'data/111.csv')
+
+# df_boxplot['day_hour'] = df_boxplot['datetime'].dt.strftime('%Y-%m-%d %H')
+
+# df_boxplot['day_count'] = df_boxplot.groupby('date')['cod'].transform('count')
+
+# df_boxplot = df_orig.copy()
+# df_boxplot['date'] = df_boxplot['datetime'].dt.strftime('%Y-%m-%d')
+# df_boxplot['weekday'] = df_boxplot['datetime'].apply(lambda x: x.weekday())
+
+# def which_week(date):
+#     for key, value in df_grouped_W:
+#         if date in list(value):
+#             a = str(key)[0:10]
+#     return a
+
+
+# df_boxplot['week'] = df_boxplot['date'].apply(lambda x: which_week(x))
+# df_boxplot['which_day'] = df_boxplot['weekday'].apply(
+#     lambda x: 'workday' if x < 5 else 'dayoff')
+
+# 将读取数据按周进行分组
+# df_grouped_W = df_boxplot.groupby(
+#     pd.Grouper(key='datetime', axis=0, freq='W'))['date']
+
+# 测试
+# for i in df_boxplot['work']:
+#     print(i,type(i))
